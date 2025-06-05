667d102158b4683fe789d2b058f6a2e5de528dc3
gabrel77 <gabriel_pereira156@estudante.sesisenai.org.br>
2025-06-05 20:35:45 -0300

    Update teste_legal.py

--- teste_legal.py
+++ teste_legal.py
@@ -1,4 +1,8 @@
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+#IDENTAÇÃO
+if 5>2:
+    print("eu vou ali, jajá")
+if 5>3:
+    print ("e tudo mundo morreu")
+#SIMMM
+if 10>11:
+    print ("sim, ah")
